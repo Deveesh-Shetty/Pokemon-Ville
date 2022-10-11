6e237dd0589e49fe4918f622acd3ba945a96a51a
Deveesh Shetty <deveeshshetty@gmail.com>
2022-10-11 18:55:25 +0530

    Minor Changes

--- src/Components/Pokemon/Components/PokeImage.jsx
+++ src/Components/Pokemon/Components/PokeImage.jsx
@@ -1,21 +1,19 @@
 import React from "react";
-import styles from "../Pokemon.css";
+import { useState } from "react";
+import "../Pokemon.css";
 
 function PokeImage({ image, name, otherImages }) {
+  const [active, setActive] = useState(null);
   return (
-    <>
-      <img
-        src={image.front_default}
-        alt={name}
-        className={styles["pokemon-image"]}
-      />
+    <div className="image-container">
+      <img src={image.front_default} alt={name} className="pokemon-image" />
       <div>
         <img src={otherImages.front_default} alt={`Front ${name}`} />
         <img src={otherImages.back_default} alt={`Back ${name}`} />
         <img src={otherImages.front_shiny} alt={`Front Shiny ${name}`} />
         <img src={otherImages.back_shiny} alt={`Back Shiny ${name}`} />
       </div>
-    </>
+    </div>
   );
 }
 
