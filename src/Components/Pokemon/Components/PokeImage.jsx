37d880539e2ed325dd5b85ae48677287b8a6fb4c
Deveesh Shetty <deveeshshetty@gmail.com>
2022-11-21 21:34:28 +0530

    Added some CSS to the individual pokemon page

--- src/Components/Pokemon/Components/PokeImage.jsx
+++ src/Components/Pokemon/Components/PokeImage.jsx
@@ -1,20 +1,20 @@
-import React from "react";
-import { useState } from "react";
-import "../Pokemon.css";
+import React from "react"
+import { useState } from "react"
+import "../Pokemon.css"
 
 function PokeImage({ image, name, otherImages }) {
-  const [active, setActive] = useState(null);
+  const [active, setActive] = useState(null)
   return (
     <div className="image-container">
       <img src={image.front_default} alt={name} className="pokemon-image" />
-      <div>
+      <div className="image-set">
         <img src={otherImages.front_default} alt={`Front ${name}`} />
         <img src={otherImages.back_default} alt={`Back ${name}`} />
         <img src={otherImages.front_shiny} alt={`Front Shiny ${name}`} />
         <img src={otherImages.back_shiny} alt={`Back Shiny ${name}`} />
       </div>
     </div>
-  );
+  )
 }
 
-export default PokeImage;
+export default PokeImage
